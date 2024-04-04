feb3ef1e2c7dca69480194083c9b9d226d286049
meet <meetbhalodiya1030@gmail.com>
2024-04-04 22:51:55 +0530

    update: start stop triggers

--- src/scroll.js
+++ src/scroll.js
@@ -8,17 +8,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	gsap.to(".box", { y: 30, yoyo: true, duration: 1, repeat: -1 });
 
-	gsap.to("#box-D", {
+	// gsap.to("#box-D", {
+	// 	scrollTrigger: {
+	// 		trigger: "#box-D",
+	// 		// first keyword - for enters viewport
+	// 		// second keyword - for exits viewport
+	// 		// first keyword - for enters back from bottom viewport
+	// 		// fourth keyword - pata nai did get it
+	// 		toggleActions: "play pause reverse pause", //play pause resume reverse restart reset complete none
+	// 		markers: true,
+	// 		start: "top center", //first is the element itself second is the viewport
+	// 		end: "bottom 20%",
+	// 	},
+	// 	x: 400,
+	// 	rotation: 360,
+	// 	duration: 1,
+	// });
+	gsap.to("#box-C", {
 		scrollTrigger: {
-			trigger: "#box-D",
-			// first keyword - for enters viewport
-			// second keyword - for exits viewport
-			// first keyword - for enters back from bottom viewport
-			// fourth keyword - pata nai did get it
+			trigger: "#box-B",
+			endTrigger: "#box-D",
 			toggleActions: "play pause reverse pause", //play pause resume reverse restart reset complete none
 			markers: true,
-			start: "top center", //first is the element itself second is the viewport
-			end: "bottom 20%",
+			start: "bottom 500px", //first is the element itself second is the viewport
+			end: "top 20%",
 		},
 		x: 400,
 		rotation: 360,
